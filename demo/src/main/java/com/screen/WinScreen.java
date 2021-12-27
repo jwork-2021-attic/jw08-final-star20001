55b69f86ba1844ba866d8a1939fe2cb82da3275b
star20001 <liuruizhe888@126.com>
2021-12-27 16:36:27 +0800

    maven
    use maven

---
+++ demo/src/main/java/com/screen/WinScreen.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2015 Aeranythe Echosong
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.screen;
+import java.io.IOException;
+import com.asciiPanel.AsciiPanel;
+
+
+/**
+ *
+ * @author Aeranythe Echosong
+ */
+public class WinScreen extends RestartScreen {
+
+    private long score;
+    private String result = "";
+
+    private int flag=0;
+
+    public WinScreen(long score) {
+        this.score = score;
+        if (this.result == "" && flag == 0) {
+            System.out.println("Get Result");
+            getresult();
+            flag = 1;
+            System.out.println("Got");
+        }
+    }
+
+    private void getresult() {
+        try {
+            Client client = new Client(score);
+            this.result = client.getResult();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    @Override
+    public Screen displayOutput(AsciiPanel terminal) {
+        String message = String.format("You win! Your time-cost are %d", score);
+        terminal.write(message, 0, 0);
+        terminal.write(result, 0, 1);
+        return this;
+    }
+
+}
---
+++ demo/src/main/java/com/world/BossAI.java
@@ -0,0 +1,60 @@
+package com.world;
+import java.util.Random;
+
+public class BossAI extends CreatureAI {
+
+    private Creature player;
+
+    public BossAI(Creature creature, Creature player) {
+        super(creature);
+        this.player = player;
+    }
+
+    @Override
+    public void onEnter(int x, int y, Tile tile) {
+        if (tile.isGround()) {
+            creature.setX(x);
+            creature.setY(y);
+        }
+    }
+
+    public void onUpdate() {
+    }
+
+    private int getdirection(int x, int tx) {
+        if (x == tx) {
+            return 0;
+        }
+        if (x < tx) {
+            return 1;
+        }
+        return -1;
+    }
+    @Override
+    public void run(){
+        while(this.creature.hp()>0){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int dx, dy;
+            dx = getdirection(this.creature.x(), player.x());
+            dy = getdirection(this.creature.y(), player.y());
+            if (dx == 0 || dy == 0) {
+                this.creature.moveBy(dx, dy);
+            }
+            else {
+                Random rand = new Random();
+                switch (rand.nextInt(2)) {
+                case 0:
+                    this.creature.moveBy(dx, 0);
+                    break;
+                case 1:
+                    this.creature.moveBy(0, dy);
+                    break;
+                }
+            }
+        }
+    }
+}
